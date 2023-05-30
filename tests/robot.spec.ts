ce2b0c6e260fa49f41b94d66ce359a1cf841b512
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-05-30 15:57:05 +0200

    add tests for robot movements

--- tests/robot.spec.ts
+++ tests/robot.spec.ts
@@ -1,7 +1,82 @@
-import { describe, expect, it } from "@jest/globals";
+import { beforeEach, describe, expect, it } from "@jest/globals";
 
-describe("Rove", () => {
-  it("should be true", () => {
-    expect(true).toBeTruthy();
+import { Orientation } from "../src/enums/index";
+import { Coordinate, Planet, Robot } from "../src/classes/index";
+
+describe("Rover", () => {
+  let planet: Planet, rover: Robot;
+
+  beforeEach(() => {
+    planet = new Planet(5);
+    const coords = new Coordinate(1, 1);
+    rover = new Robot(coords, Orientation.NORTH, planet);
+  });
+
+  it("doit avancer vers le nord", () => {
+    rover.advance();
+    expect(rover.coordinate.y).toEqual(2);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.NORTH);
+  });
+
+  it("doit faire le tour de la planète", () => {
+    for (let i = 1; i <= planet.size; i++) {
+      rover.advance();
+    }
+    expect(rover.coordinate.y).toEqual(1);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.NORTH);
+  });
+
+  it("doit reculer vers le sud", () => {
+    rover.moveBack();
+    expect(rover.coordinate.y).toEqual(5);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.NORTH);
+  });
+
+  it("doit faire demi-tour et avancer vers le sud", () => {
+    rover.turnRight();
+    rover.turnRight();
+    rover.advance();
+    expect(rover.coordinate.y).toEqual(5);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.SOUTH);
+  });
+
+  it("doit tourner à droite et avancer vers l'est", () => {
+    rover.turnRight();
+    rover.advance();
+    expect(rover.coordinate.y).toEqual(1);
+    expect(rover.coordinate.x).toEqual(2);
+    expect(rover.orientation).toEqual(Orientation.EAST);
+  });
+
+  it("doit tourner à gauche et avancer vers l'ouest", () => {
+    rover.turnLeft();
+    rover.advance();
+    expect(rover.coordinate.y).toEqual(1);
+    expect(rover.coordinate.x).toEqual(5);
+    expect(rover.orientation).toEqual(Orientation.WEST);
+  });
+
+  it("doit tourner sur lui-même par la gauche et être orienté vers le nord", () => {
+    rover.turnLeft();
+    rover.turnLeft();
+    rover.turnLeft();
+    rover.turnLeft();
+    expect(rover.coordinate.y).toEqual(1);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.NORTH);
+  });
+
+  it("doit tourner sur lui-même par la droite et être orienté vers le nord", () => {
+    rover.turnRight();
+    rover.turnRight();
+    rover.turnRight();
+    rover.turnRight();
+    expect(rover.coordinate.y).toEqual(1);
+    expect(rover.coordinate.x).toEqual(1);
+    expect(rover.orientation).toEqual(Orientation.NORTH);
   });
 });
